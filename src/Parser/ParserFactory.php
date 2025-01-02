ed51fb6ef1264d3226ada49495a88126413394ad
Jeroen de Graaf <hello@jero.work>
2025-01-02 19:06:19 +0100

    Setup factory to create Parser

---
+++ src/Parser/ParserFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jerowork\GraphqlAttributeSchema\Parser;
+
+use Jerowork\GraphqlAttributeSchema\Parser\NodeParser\Child\ClassFieldNodesParser;
+use Jerowork\GraphqlAttributeSchema\Parser\NodeParser\Child\MethodArgNodesParser;
+use Jerowork\GraphqlAttributeSchema\Parser\NodeParser\EnumNodeParser;
+use Jerowork\GraphqlAttributeSchema\Parser\NodeParser\InputTypeNodeParser;
+use Jerowork\GraphqlAttributeSchema\Parser\NodeParser\MutationNodeParser;
+use Jerowork\GraphqlAttributeSchema\Parser\NodeParser\QueryNodeParser;
+use Jerowork\GraphqlAttributeSchema\Parser\NodeParser\TypeNodeParser;
+use Jerowork\GraphqlAttributeSchema\Util\Finder\Native\NativeFinder;
+use Jerowork\GraphqlAttributeSchema\Util\Reflector\Roave\RoaveReflector;
+
+final readonly class ParserFactory
+{
+    public static function create(): Parser
+    {
+        $methodArgNodesParser = new MethodArgNodesParser();
+        $classFieldNodesParser = new ClassFieldNodesParser($methodArgNodesParser);
+
+        return new Parser(
+            new NativeFinder(),
+            new RoaveReflector(),
+            [
+                new EnumNodeParser(),
+                new InputTypeNodeParser($classFieldNodesParser),
+                new TypeNodeParser($classFieldNodesParser),
+                new MutationNodeParser($methodArgNodesParser),
+                new QueryNodeParser($methodArgNodesParser),
+            ],
+        );
+    }
+}
