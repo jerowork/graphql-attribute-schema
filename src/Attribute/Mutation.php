3a5320e37ead683e55e333d940918093029622b7
Jeroen de Graaf <hello@jero.work>
2025-05-28 17:19:20 +0200

    Extend attributes with optional `deferredTypeLoader` argument
    To be used to define the `DeferredTypeLoader` implementation for a
    field or resolver (query or mutation).

--- src/Attribute/Mutation.php
+++ src/Attribute/Mutation.php
@@ -7,18 +7,21 @@ namespace Jerowork\GraphqlAttributeSchema\Attribute;
 use Attribute;
 use Jerowork\GraphqlAttributeSchema\Attribute\Option\ScalarType;
 use Jerowork\GraphqlAttributeSchema\Attribute\Option\Type;
+use Jerowork\GraphqlAttributeSchema\Type\Loader\DeferredTypeLoader;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 final readonly class Mutation implements NamedAttribute, TypedAttribute
 {
     /**
      * @param null|class-string|Type|ScalarType $type
+     * @param null|class-string<DeferredTypeLoader> $deferredTypeLoader
      */
     public function __construct(
         public ?string $name = null,
         public ?string $description = null,
         public null|ScalarType|string|Type $type = null,
         public ?string $deprecationReason = null,
+        public ?string $deferredTypeLoader = null,
     ) {}
 
     public function getName(): ?string
